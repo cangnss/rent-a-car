deefcc14157a1afafcbcadc1ab14fb6206b0952e
cangnss <cgunes52@gmail.com>
2022-09-07 23:05:50 +0300

    added rents form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
   has_one_attached :avatar
-  has_many :car
+  has_many :cars
   has_many :rents
+  has_many :rented_cars, through: :rents, source: :cars # bir kullanıcı birden çok araba kiralayabilir.
   
   validates :name, :surname, :age, :avatar, presence: true
   validates :age, length:{ in: 0..90}
